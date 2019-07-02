cb0e4e967ca9614572016ebdde4f862d2cbac148
Laboratório de Redes <labredes@Labredes.pucrs.br>
2019-07-01 21:50:06 -0300

    update

--- dhcp_sniffer.py
+++ dhcp_sniffer.py
@@ -27,12 +27,38 @@ def get_arp_opcode(opc):
     elif opc==2: return 'reply'
     else: return 'unlisted opcode ' + str(opc)
 
+def mac2bytes(mac):
+  bytes = ''
+  for c in mac.split(':'):
+    bytes = bytes + c.decode('hex')
+  return bytes
 
+def string2hexip(sip):
+  ns = sip.split('.')
+  ns2 = []
+  for n in ns:
+    ns2.append(int(n))
+  return struct.pack('!BBBB', ns2[0], ns2[1], ns2[2], ns2[3])
 
+ip_offer = string2hexip('10.32.143.150')
+ip_a = string2hexip('10.32.143.66')
+mac_a = mac2bytes('a4:1f:72:f5:90:80')
+subnet_mask = string2hexip('255.255.255.0')
+zero_char = chr(0)
 
+ip_ver_send = 4
+ip_ihl_send = 5
+ip_ihl_ver = (ip_ver << 4) + ip_ihl_send
+ip_tos_send = 0
+ip_tot_len_send = 0  # kernel will fill the correct total length
+ip_id_send = 54321   #Id of this packet
+ip_frag_off_send = 0
+ip_ttl_send = 255
+ip_proto_send = socket.IPPROTO_ICMP
+ip_check_send = 0    # kernel will fill the correct checksum
 
-
-
+#TODO
+ip_check = 0
 
 
 def main():
@@ -95,16 +121,62 @@ def main():
                     next_start = udp_start + udp_length
                     next_size = total_udp_length - udp_length
                     rem_packet = packet[next_start : next_start+next_size]
-                    
-                    dhcp_option = rem_packet[59:64]
-                    
-                    
-                    print(total_udp_length)
-                    i = 0
-                    while(i<len(rem_packet)):
-                        print(i, rem_packet[i:i+4])
-                        i+=4
-                    print(i, rem_packet[i:i+len(rem_packet)%4])
+                    dhcp_xid = rem_packet[4:8]
+                    dhcp_option = rem_packet[240:243]
+                    # Discover
+                    if dhcp_option[0] == 0x35 and dhcp_option[2] == 0x1:
+                        print(eth_srcaddr)
+                        
+                        #===========================
+                        eth_send = struct.pack("!6s6sH", eth_srcaddr, mac_a, eth_type)
+                        ip_send = struct.pack('!BBHHHBBH4s4s', version_ihl, 0, ????, , 0, 0, 255, protocol, ip_check, ip_a, d_addr)
+                        udp_send = struct.pack('!HHHH', dest_port, source_port, 308, 0)
+                        rem_packet_send = struct.pack('',
+                                                      0x02010600, # 4B
+                                                      dhcp_xid, # 4B
+                                                      0, #secs 2B
+                                                      0, #flag 2B
+                                                      0, #ciaddr 4B
+                                                      0, #yiaddr 4B
+                                                      0, #siaddr 4B
+                                                      0, #giaddr 4B
+                                                      eth_srcaddr, #chaddr 4B
+                                                      0x0000, #2B
+                                                      0, #4B
+                                                      0, #4B
+                                                      zero_char*192 #192B
+                                                      0x63825363 #magic cookie 4B
+                                                      53, #dhcp msg #1B
+                                                      1, #length 1B
+                                                      2, #opt data 1B
+                                                      1, #opt mask 1B
+                                                      4, #length 1B
+                                                      subnet_mask, #4B
+                                                      , #TODO
+                                                      51, #lease ip time 1B
+                                                      4, #len 1B
+                                                      120, #2 minutos 4B
+                                                      )
+                        ip_header = struct.pack('!BBHHHBBH4s4s' , ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
+                        
+                        
+                        eth_header = struct.pack("!6s6sH", target_mac, source_mac, eth_prot)
+                        
+                        
+                        
+                        
+                        
+                        
+                        #===========================
+                        
+                    # Request
+                    if dhcp_option[0] == 0x35 and dhcp_option[2] == 0x3:
+                        print(dhcp_option)
+                        
+                    # NAK
+                    if dhcp_option[0] == 0x35 and dhcp_option[2] == 0x6:
+                        print(dhcp_option)
+                        
                     print('======================================\n\n')
                     
                 
